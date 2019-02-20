949b1c61d8f3cf67c17988914ba93a76f32a64d1
Irina Kukuyeva <ikukuyeva@gmail.com>
2019-02-19 18:48:28 -0800

    Class6: Add logging to script

--- Class6/Intro-to-AWS-S3.py
+++ Class6/Intro-to-AWS-S3.py
@@ -8,16 +8,23 @@
 # - Creating and adding AWS region for computing resources in `.aws/config` file
 
 import boto3
+import logging
 import joblib
 import pandas as pd
 
 # Documentation: https://s3fs.readthedocs.io/en/latest/?badge=latest
 # Note: s3fs is a wrapper for boto3
-import s3fs 
+import s3fs
+
+logging.basicConfig(level=logging.INFO)
+# Define one logger for current file, per
+# https://www.loggly.com/blog/4-reasons-a-python-logging-library-is-much-better-than-putting-print-statements-everywhere/
+LOGGER = logging.getLogger(__name__)
 
 ### ---------------------------------------------------------------------------
 ### --- Part 1: Connect to S3 Bucket on AWS
 ### ---------------------------------------------------------------------------
+LOGGER.info("--- Part 1: Connect to S3 Bucket on AWS")
 
 # Approach 1: 
 s3 = boto3.resource('s3')
@@ -28,18 +35,24 @@ s3_fs = s3fs.S3FileSystem(anon=False)
 
 
 # View list of all buckets available on AWS. Note: These are mine -- yours will differ:
+LOGGER.info("List of buckets currently available on AWS S3:")
 for bucket in s3.buckets.all():
-    print(bucket.name)
+    LOGGER.info(f"    {bucket.name}")
 
 
-# View list of objects in given bucket:
-for file in s3.Bucket('stats404-project').objects.all():
-    print(file.key)
+# View list of objects in given bucket, named 'bucket_name':
+bucket_name = 'stats404-project'
+LOGGER.info(f"List of objects in bucket {bucket_name}:")
+for file in s3.Bucket(bucket_name).objects.all():
+    LOGGER.info(f"    {file.key}")
+
 
 ### ---------------------------------------------------------------------------
 ### --- Part 2: Upload CSV File to S3 Bucket
 ### ---------------------------------------------------------------------------
+LOGGER.info("--- Part 2: Upload CSV File to S3 Bucket")
 
+LOGGER.info("    Download 1000 rows of Airline flight paths")
 # --- Step 1: Create a data set to upload -- or use one for your project:
 file_name = "https://s3.amazonaws.com/h2o-airlines-unpacked/year1987.csv"
 df = pd.read_csv(filepath_or_buffer=file_name,
@@ -47,36 +60,39 @@ df = pd.read_csv(filepath_or_buffer=file_name,
                  nrows=1000
                 )
 
-# --- Step 2: Specify name of bucket to upload to:
-bucket_name = "stats404-project"
-
-# --- Step 3: Specify name of file to be created on s3, to store this CSV:
+# --- Step 2: Specify name of file to be created on s3, to store this CSV:
 key_name = "airlines_data_1987_1000rows.csv"
 
-# --- Step 4: Upload file to bucket and file name specified: 
+# --- Step 3: Upload file to (same) bucket and file name specified:
+LOGGER.info(f"    Uploading file: {key_name} to S3 bucket = {bucket_name}")
 with s3_fs.open(f"{bucket_name}/{key_name}","w") as file:
     df.to_csv(file)
+LOGGER.info(f"    Uploaded file: {key_name} to S3 bucket = {bucket_name}")
 
-# --- Step 5: Check that file got uploaded:
-for file in s3.Bucket('stats404-project').objects.all():
-    print(file.key)
+# --- Step 4: Check that file got uploaded:
+LOGGER.info(f"List of objects in bucket {bucket_name} now:")
+for file in s3.Bucket(bucket_name).objects.all():
+    LOGGER.info(f"    {file.key}")
 
 ### ---------------------------------------------------------------------------
 ### --- Part 3: Upload Model Object to S3 Bucket
 ### ---------------------------------------------------------------------------
+LOGGER.info("--- Part 3: Upload Model Object to S3 Bucket")
 
 # --- Step 1: Load a previously estimated model object in workspace:
-rf_dict = joblib.load("../Class4/rf.joblib") 
+LOGGER.info("    Loading RF model object")
+rf_dict = joblib.load("../Class4/rf.joblib")
 
-# --- Step 2: Keep bucket the same
-
-# --- Step 3: Specify name of file to be created on s3, to store this model object:
+# --- Step 2: Specify name of file to be created on s3, to store this model object:
 key_name = "rf_Fashion_MNIST_500_trees.joblib"
 
-# --- Step 4: Upload file to bucket and file name specified:
+# --- Step 3: Upload file to bucket and file name specified:
+LOGGER.info(f"    Uploading file: {key_name} to S3 bucket = {bucket_name}")
 with s3_fs.open(f"{bucket_name}/{key_name}","wb") as file:
     joblib.dump(rf_dict[500], file)
+LOGGER.info(f"    Uploaded file: {key_name} to S3 bucket = {bucket_name}")
 
-# --- Step 5: Check that file got uploaded:
-for file in s3.Bucket('stats404-project').objects.all():
-    print(file.key)
+# --- Step 4: Check that file got uploaded:
+LOGGER.info(f"List of objects in bucket {bucket_name} now:")
+for file in s3.Bucket(bucket_name).objects.all():
+    LOGGER.info(f"    {file.key}")
